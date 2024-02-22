2fbae10261dc84a29a4e5e349e28f60267f89d5d
M Yaseen <yaseenghouri2@gmail.com>
2024-02-22 17:09:14 +0500

    assign 16

---
+++ app.js
@@ -0,0 +1,131 @@
+// CHAPTER 1
+
+// Q1
+alert("Welcome User")
+
+// Q2
+alert("Error! Please enter a valid password.")
+
+// Q3
+alert("Welcome to JS Land...\nHappy Coding!")
+
+// Q4
+alert("Welcome to JS Land...")
+alert("Happy Coding!\nPrevent this page from creating additional dialogs.")
+
+// Q5
+console.log ("Hello... I can run JS through my web browner's console")
+
+// CHAPTER 2
+
+// Q1
+var username;
+
+// Q2
+var myName = "M Yaseen";
+
+// Q3
+var message = "Hello World";
+alert(message)
+
+// Q4 
+var name = "Jhone Doe"; age = "15 years old"; degree = "Certified Mobile App Dev";
+alert (name)
+alert (age)
+alert (degree)
+
+// Q5
+document.write ("PIZZA<br>PIZZ<br>PIZ<br>PI<br>P")
+
+// Q6
+var email = "example@example.com";
+alert("My email address is", email)
+
+// Q7
+var book = "A smarter way to learn JawavScript";
+alert (book)
+
+// Q8
+document.write ("Yah! I can write HTML content thtough JavaScript")
+
+// Q9
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert (design)
+document.write(design)
+
+// CHAPTER 3
+
+// Q1
+var age = "I am 15 years old";
+alert (age)
+
+// Q2
+var uservisits = "You have visited this site 2 times";
+alert (uservisits)
+
+// Q3
+var birthyear = "My birth year is 1990<br>Data type of my declared variable is number";
+document.write(birthyear)
+
+// Q4
+var visitorname = "John Doe"; product = "T-shirts"; quantity = 5;
+document.write(visitorname, "ordered", quantity ,product ,"on XYZ Clothing Store")
+
+// CHAPTER 1-3
+
+// CHAPTER 1
+
+// Q1
+alert("Jhon");
+alert("Doe");
+alert("example@example.com");
+alert(31512801997);
+alert("HAHANOOB12.");
+
+// Q2
+alert("You're learning JavaScript!");
+
+// Q3
+alert("This course is good ez");
+
+// CHAPTER 2
+
+// Q1
+var myVariableName = 15;
+
+// Q2
+var game;
+var game = "cs2";
+
+// Q3
+var teamName = "Cloud9";
+
+// Q4
+var bestMan = "Charlie";
+bestMan = "John";
+
+// CHAPTER 3
+
+// Q1
+var caseQty;
+
+// Q2
+caseQty = 144;
+
+// Q3
+var num = 9;
+let result = caseQty * num;
+
+// Q4
+var x;
+x = 6 + 2;
+
+// Q5
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+// the answer is 110
+
+// Q6
+var taxRate = 7;
+taxRate = 7 + 15;
